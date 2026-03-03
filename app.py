cf5ecc39f470f7cce709c30eef7d058a266a4433
rahul <rahulshrivardhanmarisetti@gmail.com>
2026-03-03 09:12:12 +0530

    done

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-client = Groq(api_key=os.getenv("API_KEY"))
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 @app.route("/", methods=["GET", "POST"])
 def home():
